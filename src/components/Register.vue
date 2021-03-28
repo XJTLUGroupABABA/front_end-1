7a15a9d7c2ef7bdfd7c2406e75370380a2a6de4b
名称 <kfscly@163.com>
2021-03-28 18:39:11 +0800

    注册界面翻新
    增加对注册信息的限制
    可以选择密码隐藏/可视功能

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -12,27 +12,45 @@
           v-model.trim="name"
           placeholder="Please enter your user name"
           onKeyUp="value=value.replace(/[\W]/g,'')"
+          maxlength="20"
         /><br />
       </div>
-      <div class="form">
+      <div class="passWord">
         <input
-          type="password"
+          :type="pwdType"
           v-model.trim="password"
           placeholder="Please enter your password"
-        /><br />
+          onkeyup="this.value=this.value.replace(/[^\w]/g,'')"
+          maxlength="10"
+        />
+        <img
+          :src="seen ? openeye : nopeneye"
+          class="eye"
+          alt=""
+          @click="changeType()"
+        />
       </div>
       <div class="form">
         <input
-          type="password"
+          :type="pwdType1"
           v-model.trim="passwordAgain"
           placeholder="Please enter the password again"
-        /><br />
+          onkeyup="this.value=this.value.replace(/[^\w]/g,'')"
+          maxlength="10"
+        />
+        <img
+          :src="seen1 ? openeye : nopeneye"
+          class="eye"
+          alt=""
+          @click="changeType2()"
+        />
       </div>
       <div class="form">
         <input
-          type="email"
+          type="phone"
           v-model.trim="phone"
           placeholder="Please enter your registration phone"
+          onkeyup="this.value=this.value.replace(/\D/g,'')"
         /><br />
       </div>
       <button @click.prevent="handlefinish">REGISTER</button>
@@ -106,14 +124,21 @@ button {
   height: 50px;
   line-height: 26px;
   border-radius: 30px;
-  margin-top:40px;
+  margin-top: 40px;
   background-color: rgba(210, 111, 108, 100);
   color: rgba(255, 255, 255, 100);
   font-size: 18px;
   text-align: center;
   font-family: Roboto;
   border: 1px solid rgba(210, 111, 108, 100);
-  margin-left:30%;
+  margin-left: 30%;
+}
+
+img {
+  float: right;
+  height: 26px;
+  margin-top: 57px;
+  margin-right: 20px;
 }
 </style>
 
@@ -129,28 +154,46 @@ export default {
       password: "",
       passwordAgain: "",
       phone: "",
+      seen: "",
+      seen1: "",
+      pwdType: "password", // 密码类型
+      pwdType1: "password", // 密码类型
+      openeye: require("../assets/eye.png"), //图片地址
+      nopeneye: require("../assets/eye2.png"), //图片地址
     };
   },
+
   methods: {
+    changeType() {
+      this.pwdType = this.pwdType === "password" ? "text" : "password";
+      this.seen = !this.seen; //小眼睛的变化
+    },
+    changeType2() {
+      this.pwdType1 = this.pwdType1 === "password" ? "text" : "password";
+      this.seen1 = !this.seen1; //小眼睛的变化
+    },
+
     //点击完成按钮触发handlefinish
     handlefinish: function () {
       if (localStorage["name"] === this.name) {
-        alert("用户名已存在"); //如果用户名已存在则无法注册
+        alert("The user name has existed, please login directly"); //如果用户名已存在则无法注册
       } else if (this.name === "") {
-        alert("用户名不能为空");
-      }
-      else if (this.name == "this.value=this.value.replace(/[^\u4e00-\u9fa5]/g,'')") {
-        alert("用户名不能为中文");
+        alert("Sorry, the user name cannot be empty"); //用户名不能为空
+      } else if (this.password.length <= "6") {
+        alert("Please enter more than six digits"); //密码必须超过六位
+      } else if (this.phone.length !== 11) {
+        alert("Please enter your phone number within 11 digits"); //手机号必须为11位
+      } else if (this.passwordAgain !== this.password) {
+        alert("Please keep the same password");  //两次密码保持一致
       }
       else {
         //将新用户信息存储到localStorage
         localStorage.setItem("name", this.name);
         localStorage.setItem("password", this.password);
-        localStorage.setItem("mail", this.passwordAgain);
-        localStorage.setItem("tel", this.phone);
+        localStorage.setItem("phone", this.phone);
         localStorage.setItem("s", "false");
-        alert("注册成功");
-        this.$router.replace("/page_contact"); //完成注册后跳转至contact页面
+        alert("Congratulate! You have registered successfully");
+        this.$router.replace("../components/page_contact"); //完成注册后跳转至contact页面
       }
     },
   },
